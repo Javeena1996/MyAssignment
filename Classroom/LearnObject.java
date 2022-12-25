417ca31eb5c976890fc8cf3cd09a69382267e379
Javeena1996 <120119655+Javeena1996@users.noreply.github.com>
2022-12-25 12:26:44 +0530

    Add files via upload

---
+++ Classroom/LearnObject.java
@@ -0,0 +1,25 @@
+package week1.day1;
+
+public class LearnObject {
+	
+	public void driveCar() {
+		
+		System.out.println("Start the Car");
+
+			}
+			
+			public void applyBrake() {
+				
+		System.out.println("Stop the Car");
+		
+			}
+
+	public static void main(String[] args) {
+        LearnObject c1=new LearnObject();
+        c1.driveCar();
+        c1.applyBrake();
+
+		
+	}
+
+}
