049e88735dbc4dcf0b9dfbc2a1b134a2c17bd37e
Javeena1996 <120119655+Javeena1996@users.noreply.github.com>
2022-12-25 12:39:17 +0530

    Add files via upload

---
+++ Assignment5/Student.java
@@ -0,0 +1,14 @@
+package week1.day1;
+
+public class Student {
+
+	
+			String studentName= "Javeena";
+			int rollNo= 19;
+			String college= "PSG CAS";
+			int markScored= 85;
+			float CGPA= 8.1f;
+		
+	
+
+}
